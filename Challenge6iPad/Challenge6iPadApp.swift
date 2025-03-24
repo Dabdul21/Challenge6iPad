6ab8ebbc79f4de179d50c0d768386a5cb0ffbb48
Dayan A <codayanabdulla@gmail.com>
2025-03-24 09:03:30 -0400

    Initial Commit

---
+++ Challenge6iPad/Challenge6iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge6iPadApp.swift
+//  Challenge6iPad
+//
+//  Created by Dayan Abdulla on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Challenge6iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
