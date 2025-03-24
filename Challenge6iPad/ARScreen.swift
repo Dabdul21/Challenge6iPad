03a1b2ea7c8582cdc2b42af14ef4b6a6d1d1f834
Dayan A <codayanabdulla@gmail.com>
2025-03-24 09:12:10 -0400

    added logo and ARView

---
+++ Challenge6iPad/ARScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ARScreen.swift
+//  Challenge6iPad
+//
+//  Created by Dayan Abdulla on 3/24/25.
+//
+
+import SwiftUI
+
+struct ARScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARScreen()
+}
