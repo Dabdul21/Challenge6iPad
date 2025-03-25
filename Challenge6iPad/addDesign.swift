4b23f8ebad8c4a3e6fee702ed23707f316a029f1
Dayan A <codayanabdulla@gmail.com>
2025-03-25 08:45:49 -0400

    Finished "welcome screen" View  getting started on addDesign View

---
+++ Challenge6iPad/addDesign.swift
@@ -0,0 +1,18 @@
+//
+//  addDesign.swift
+//  Challenge6iPad
+//
+//  Created by Dayan Abdulla on 3/25/25.
+//
+
+import SwiftUI
+
+struct addDesign: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addDesign()
+}
