f189789348d15007d3d81101d924bba70e15d84e
Dayan A <codayanabdulla@gmail.com>
2025-03-25 08:51:21 -0400

    fixes

--- Challenge6iPad/addDesign.swift
+++ Challenge6iPad/addDesign.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct addDesign: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    Color(red: 0/255, green: 0/255, blue: 0/255),           // almost black
+
+                    
+                    Color(red: 0/255, green: 40/255, blue: 94/255),         // deep navy blue
+                    Color(red: 85/255, green: 142/255, blue: 235/255),      // sky blue
+                    Color(red: 160/255, green: 180/255, blue: 255/255)      // light purplish blue
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("Add Design ")
+                    .font(.system(size: 40, weight: .bold, design: .default))
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(30)
+                
+            }
+        }
     }
 }
 
--- Challenge6iPad/startScreen.swift
+++ Challenge6iPad/startScreen.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct startScreen: View {
     var body: some View {
-        
+        NavigationStack{
         ZStack {
             LinearGradient(
                 gradient: Gradient(colors: [
                     Color(red: 0/255, green: 0/255, blue: 0/255),           // almost black
+
+                    
                     Color(red: 0/255, green: 40/255, blue: 94/255),         // deep navy blue
                     Color(red: 85/255, green: 142/255, blue: 235/255),      // sky blue
                     Color(red: 160/255, green: 180/255, blue: 255/255)      // light purplish blue
@@ -23,18 +25,67 @@ struct startScreen: View {
             )
             .edgesIgnoringSafeArea(.all)
 
-            ZStack{
+            
+            VStack(spacing: 10){
                 Image("Logo")
                     .resizable()
-                    .frame(width: 300, height: 300)
+                    .frame(width: 250, height: 250) //img size
+                    .padding(.bottom, 20)
                 
-            
-                VStack{
-//                   Button(action:  {
-//                      {
-//                    }
-//                    }
-                        
+                Text("Casa Love")
+                    .font(.system(size: 48, weight: .medium))
+                    .padding(.bottom, 100)
+                    .foregroundColor(.white)
+
+                
+                NavigationLink(destination: Dashboard()) {
+                    HStack{
+                        Text("Browse Clothing")
+                            .font(.system(size: 48, weight: .bold))
+                        Spacer()
+                        Image(systemName: "arrow.right")
+                            .font(.system(size: 48))
+
+                    }
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 150)
+                        .padding(.vertical, 45)
+                        .background(Color(red: 31/255, green: 46/255, blue: 58/255))
+                        .cornerRadius(25)
+
+                        . overlay(
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(Color.white, lineWidth: 6) // white outline background
+                        )
+                    
+                        }
+                    .padding(.horizontal, 100) // keeps the btn from touching the edge of screen
+                    .padding(.bottom, 35)       //space in between btn's
+                
+                
+                NavigationLink(destination: addDesign()) {
+                    HStack{
+                        Text("Add New Design")
+                            .font(.system(size: 48, weight: .bold))
+
+                        Spacer()
+                        Image(systemName: "plus")
+                            .font(.system(size: 48))
+
+                    }
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 150)
+                        .padding(.vertical, 45)
+                        .background(Color(red: 39/255, green: 80/255, blue: 114/255))
+                        .cornerRadius(25)
+
+                        . overlay(
+                            RoundedRectangle(cornerRadius: 25)
+                                .stroke(Color.white, lineWidth: 6) // white outline
+                        )
+                    }
+                    .padding(.horizontal, 100) // keeps the btn from touching the screen
+
                     }
                 }
             }
